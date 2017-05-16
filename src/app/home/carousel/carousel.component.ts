46eb93e12c41358b2f4d55b60890b521a46e35bb
relative-rene <rene.arellano415@gmail.com>
2017-05-16 15:10:17 -0700

    adds image model

--- src/app/home/carousel/carousel.component.ts
+++ src/app/home/carousel/carousel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Image } from 'app/_interfaces/images.model';
 
 @Component({
   selector: 'app-carousel',
@@ -6,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carousel.component.css']
 })
 export class CarouselComponent implements OnInit {
-
+images: Image[] = [
+  new Image('BayGrits & Eggs', 'BayGrits & Eggs', './assets/baygritsEggs.jpg'),
+  new Image('Busser Area', 'Deep Scuba Busser', './assets/assetsbusArea'),
+  new Image('Spammasubi', 'Wrapped Spam', './assets/spammasubi.jpg'),
+  new Image('Fried Rolls', 'Deepfried rolls w/ dipping sauce', './assets/friedrolls.jpg'),
+  new Image('Waffle Art', 'Upclose Waffle', './assets/prowaffle.jpg'),
+  new Image('Green Latte', 'Caffeine Leaf Art', './assets/gLatte.jpg'),
+  new Image('Grits & Eggs', 'Bay shrimp and grits', './assets/gritsEggs.jpg'),
+  new Image('Chicken and Waffles', 'Chix and Waffles', './assets/waffleChix.jpg'),
+  new Image('Latte', 'Latte Art', './assets/latteArt.jpg')
+];
   constructor() { }
 
   ngOnInit() {
