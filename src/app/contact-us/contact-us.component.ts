6ab33a995fc7aa3e86cc3d84e0354989e0b530bf
relative-rene <rene.arellano415@gmail.com>
2017-06-14 14:18:19 -0700

    use service to temp log message

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -20,40 +20,23 @@ export class ContactUsComponent implements OnInit {
     private router: Router) { }
 
   ngOnInit() {
-    this.route.params
-      .subscribe((params: Params) => {
-        this.id = +params['id'];
-        this.editMode = params['id'] != null;
         this.initForm();
-      });
   }
 
   onSubmit() {
-    // const newMessage = new Message(
-    //   this.messageForm.value['email'],
-    //   this.messageForm.value['description']);
-    if (this.editMode) {
-      this.messageService.updateMessage(this.id, this.messageForm.value);
-    } else {
       this.messageService.addMessage(this.messageForm.value);
+      console.log('message added', this.messageForm.value, this.id);
+      console.log(this.messageService.getMessages());
+      this.initForm();
     }
-    this.onCancel();
-  }
 
-  onCancel() {
-    this.router.navigate(['../'], { relativeTo: this.route });
-  }
+    getMessages() {
+      console.log(this.messageService.getMessages());
+    }
 
   private initForm() {
     let messageEmail = '';
     let messageDescription = '';
-
-    if (this.editMode) {
-      const message = this.messageService.getMessage(this.id);
-      messageEmail = message.email;
-      messageDescription = message.description;
-    }
-
     this.messageForm = new FormGroup({
       'email': new FormControl(messageEmail, Validators.required),
       'description': new FormControl(messageDescription, Validators.required)
